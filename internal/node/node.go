7b828ebe498be2a5ed9f5a2f9b0a053276200f94
Aleksey Marin <asmarin@avito.ru>
2025-12-06 10:52:46 +0300

    накрыл тестами Node

---
+++ internal/node/node.go
@@ -0,0 +1,198 @@
+package node
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/amarin/binutils"
+)
+
+type (
+	// AlphabetSize задаёт максимальную размерность алфавита
+	AlphabetSize interface {
+		~uint8 | ~uint16 | ~uint32
+	}
+
+	// MorphemesMaxCount задаёт максимальную размерность словаря с учётом всех возможных словоформ
+	MorphemesMaxCount interface {
+		~uint16 | ~uint32
+	}
+)
+
+// Node задаёт структуру хранения данных об узле DAG.
+type Node[A AlphabetSize, M MorphemesMaxCount] struct {
+	idx     M
+	prev    M
+	charIdx A
+	next    map[A]M
+}
+
+// New создаёт новый узел DAG c индексом idx, индексом символа charIdx и индексом предыдущего узла prev.
+func New[A AlphabetSize, M MorphemesMaxCount](idx M, charIdx A, prev M) *Node[A, M] {
+	return &Node[A, M]{
+		idx:     idx,
+		charIdx: charIdx,
+		prev:    prev,
+		next:    make(map[A]M),
+	}
+}
+
+// Next создаёт новый узел-потомок с индексом idx и индексом символа charIdx, добавляя его в список потомков текущего.
+func (node *Node[A, M]) Next(idx M, charIdx A) *Node[A, M] {
+	node.next[charIdx] = idx
+	return New(idx, charIdx, node.idx)
+}
+
+// PrevIdx возвращает индекс предыдущего узла.
+func (node *Node[A, M]) PrevIdx() M {
+	return node.prev
+}
+
+// NextIdx возвращает индекс потомка с заданным индексом символа.
+func (node *Node[A, M]) NextIdx(charIdx A) (M, bool) {
+	next, ok := node.next[charIdx]
+	return next, ok
+}
+
+// HasNext возвращает true, если зарегистрирован потомок с заданным индексом символа.
+func (node *Node[A, M]) HasNext(charIdx A) bool {
+	_, ok := node.next[charIdx]
+	return ok
+}
+
+// CharIdx возвращает индекс символа в алфавите.
+func (node *Node[A, M]) CharIdx() A {
+	return node.charIdx
+}
+
+// WriteTo записывает бинарное представление узла в заданный io.Writer.
+func (node *Node[A, M]) WriteTo(w io.Writer) (bytesWritten int64, err error) {
+	var (
+		idx     any = node.idx
+		prev    any = node.prev
+		charIdx any = node.charIdx
+	)
+
+	bytesWritten = 0
+	res := binutils.NewBinaryWriter(w)
+
+	switch typedValue := idx.(type) {
+	case uint16:
+		if err = res.WriteUint16(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten += 2
+	case uint32:
+		if err = res.WriteUint32(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten += 4
+	}
+
+	switch typedValue := prev.(type) {
+	case uint16:
+		if err = res.WriteUint16(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten += 2
+	case uint32:
+		if err = res.WriteUint32(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten += 4
+	}
+
+	switch typedValue := charIdx.(type) {
+	case uint8:
+		if err = res.WriteUint8(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten++
+	case uint16:
+		if err = res.WriteUint16(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten += 2
+	case uint32:
+		if err = res.WriteUint32(typedValue); err != nil {
+			return bytesWritten, err
+		}
+		bytesWritten += 4
+	}
+
+	return bytesWritten, nil
+}
+
+// Bytes возвращает байтовое представление узла.
+func (node *Node[A, M]) Bytes() ([]byte, error) {
+	writer := new(bytes.Buffer)
+	if _, err := node.WriteTo(writer); err != nil {
+		return nil, err
+	}
+
+	return writer.Bytes(), nil
+}
+
+func (node *Node[A, M]) Hex() (string, error) {
+	data, err := node.Bytes()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%X", data), nil
+}
+
+// ReadFrom записывает бинарное представление узла в заданный io.Writer.
+func (node *Node[A, M]) ReadFrom(r io.Reader) (count int64, err error) {
+	var (
+		idx     any = node.idx
+		prev    any = node.prev
+		charIdx any = node.charIdx
+	)
+
+	count = 0
+	res := binutils.NewBinaryReader(r)
+
+	switch idx.(type) {
+	case uint16:
+		if idx, err = res.ReadUint16(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	case uint32:
+		if idx, err = res.ReadUint32(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	}
+	node.idx = idx.(M)
+
+	switch prev.(type) {
+	case uint16:
+		if prev, err = res.ReadUint16(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	case uint32:
+		if prev, err = res.ReadUint32(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	}
+	node.prev = prev.(M)
+
+	switch charIdx.(type) {
+	case uint8:
+		if charIdx, err = res.ReadUint8(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	case uint16:
+		if charIdx, err = res.ReadUint16(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	case uint32:
+		if charIdx, err = res.ReadUint32(); err != nil {
+			return int64(res.BytesTaken()), err
+		}
+	}
+	node.charIdx = charIdx.(A)
+
+	return int64(res.BytesTaken()), nil
+}
---
+++ internal/node/node_test.go
@@ -0,0 +1,337 @@
+package node
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func getSizeText(v any) string {
+	switch v.(type) {
+	case uint8:
+		return "uint8"
+	case uint16:
+		return "uint16"
+	case uint32:
+		return "uint32"
+	default:
+		panic(fmt.Sprintf("unexpected type: %T", v))
+	}
+}
+
+func TestNode_Hex(t *testing.T) {
+	type hexProvider interface {
+		io.ReaderFrom
+		io.WriterTo
+		Hex() (string, error)
+	}
+
+	for _, tt := range []struct {
+		prev    any    // MorphemesMaxCount
+		idx     any    // MorphemesMaxCount
+		charIdx any    // AlphabetSize
+		expect  string // HEX
+	}{
+		{
+			prev:    uint16(1),
+			idx:     uint16(2),
+			charIdx: uint8(3),
+			expect:  "0002000103",
+		},
+		{
+			prev:    uint32(1),
+			idx:     uint32(2),
+			charIdx: uint8(3),
+			expect:  "000000020000000103",
+		},
+		{
+			prev:    uint16(1),
+			idx:     uint16(2),
+			charIdx: uint16(3),
+			expect:  "000200010003",
+		},
+		{
+			prev:    uint32(1),
+			idx:     uint32(2),
+			charIdx: uint16(3),
+			expect:  "00000002000000010003",
+		},
+		{
+			prev:    uint16(1),
+			idx:     uint16(2),
+			charIdx: uint32(3),
+			expect:  "0002000100000003",
+		},
+		{
+			prev:    uint32(1),
+			idx:     uint32(2),
+			charIdx: uint32(3),
+			expect:  "000000020000000100000003",
+		},
+	} {
+		alphabetSize := getSizeText(tt.charIdx)
+		morphemesSize := getSizeText(tt.idx)
+
+		t.Run(fmt.Sprintf("алфавит %s морфемы %s", alphabetSize, morphemesSize), func(t *testing.T) {
+			var (
+				a8   uint8  = 0
+				a16  uint16 = 0
+				a32  uint32 = 0
+				i16  uint16 = 0
+				i32  uint32 = 0
+				p16  uint16 = 0
+				p32  uint32 = 0
+				node hexProvider
+				read hexProvider
+			)
+			switch typed := tt.charIdx.(type) {
+			case uint8:
+				a8 = typed
+			case uint16:
+				a16 = typed
+			case uint32:
+				a32 = typed
+			}
+
+			switch typed := tt.idx.(type) {
+			case uint16:
+				i16 = typed
+			case uint32:
+				i32 = typed
+			}
+
+			switch typed := tt.prev.(type) {
+			case uint16:
+				p16 = typed
+			case uint32:
+				p32 = typed
+			}
+
+			switch {
+			case a8 != 0 && i16 != 0:
+				node = New(i16, a8, p16)
+				read = New(uint16(0), uint8(0), uint16(0))
+			case a8 != 0 && i32 != 0:
+				node = New(i32, a8, p32)
+				read = New(uint32(0), uint8(0), uint32(0))
+			case a16 != 0 && i16 != 0:
+				node = New(i16, a16, p16)
+				read = New(uint16(0), uint16(0), uint16(0))
+			case a16 != 0 && i32 != 0:
+				node = New(i32, a16, p32)
+				read = New(uint32(0), uint16(0), uint32(0))
+			case a32 != 0 && i16 != 0:
+				node = New(i16, a32, p16)
+				read = New(uint16(0), uint32(0), uint16(0))
+			case a32 != 0 && i32 != 0:
+				node = New(i32, a32, p32)
+				read = New(uint32(0), uint32(0), uint32(0))
+			default:
+				require.Fail(t, "неожиданное сочетание типов")
+			}
+			t.Run("Hex", func(t *testing.T) {
+				hex, err := node.Hex()
+				require.NoError(t, err)
+				require.Equal(t, tt.expect, hex)
+			})
+			t.Run("WriteTo-ReadFrom", func(t *testing.T) {
+				buf := new(bytes.Buffer)
+				writeCount, err := node.WriteTo(buf)
+				require.NoError(t, err)
+				readCount, err := read.ReadFrom(buf)
+				require.NoError(t, err)
+				require.Equal(t, writeCount, readCount)
+
+				readHex, err := read.Hex()
+				require.NoError(t, err)
+				require.Equal(t, tt.expect, readHex)
+			})
+		})
+	}
+}
+
+func TestNode_Next(t *testing.T) {
+	t.Run("алфавит uint8 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint8(0), uint16(0))
+		next := node.Next(2, 0)
+		require.Equal(t, uint8(0), next.charIdx)
+		require.Equal(t, uint16(1), next.prev)
+		require.Equal(t, uint16(2), next.idx)
+	})
+	t.Run("алфавит uint8 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint8(0), uint32(0))
+		next := node.Next(2, 0)
+		require.Equal(t, uint8(0), next.charIdx)
+		require.Equal(t, uint32(1), next.prev)
+		require.Equal(t, uint32(2), next.idx)
+	})
+	t.Run("алфавит uint16 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint16(0), uint16(0))
+		next := node.Next(2, 0)
+		require.Equal(t, uint16(0), next.charIdx)
+		require.Equal(t, uint16(1), next.prev)
+		require.Equal(t, uint16(2), next.idx)
+	})
+	t.Run("алфавит uint16 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint16(0), uint32(0))
+		next := node.Next(2, 0)
+		require.Equal(t, uint16(0), next.charIdx)
+		require.Equal(t, uint32(1), next.prev)
+		require.Equal(t, uint32(2), next.idx)
+	})
+	t.Run("алфавит uint32 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint32(0), uint16(0))
+		next := node.Next(2, 0)
+		require.Equal(t, uint32(0), next.charIdx)
+		require.Equal(t, uint16(1), next.prev)
+		require.Equal(t, uint16(2), next.idx)
+	})
+	t.Run("алфавит uint32 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint32(0), uint32(0))
+		next := node.Next(2, 0)
+		require.Equal(t, uint32(0), next.charIdx)
+		require.Equal(t, uint32(1), next.prev)
+		require.Equal(t, uint32(2), next.idx)
+	})
+}
+
+func TestNode_PrevIdx(t *testing.T) {
+	t.Run("алфавит uint8 морфемы uint16", func(t *testing.T) {
+		require.Equal(t, uint16(1), New(uint16(2), uint8(0), uint16(1)).PrevIdx())
+	})
+	t.Run("алфавит uint8 морфемы uint32", func(t *testing.T) {
+		require.Equal(t, uint32(1), New(uint32(2), uint8(0), uint32(1)).PrevIdx())
+	})
+	t.Run("алфавит uint16 морфемы uint16", func(t *testing.T) {
+		require.Equal(t, uint16(1), New(uint16(2), uint16(0), uint16(1)).PrevIdx())
+	})
+	t.Run("алфавит uint16 морфемы uint32", func(t *testing.T) {
+		require.Equal(t, uint32(1), New(uint32(2), uint16(0), uint32(1)).PrevIdx())
+	})
+	t.Run("алфавит uint32 морфемы uint16", func(t *testing.T) {
+		require.Equal(t, uint16(1), New(uint16(2), uint32(0), uint16(1)).PrevIdx())
+	})
+	t.Run("алфавит uint32 морфемы uint32", func(t *testing.T) {
+		require.Equal(t, uint32(1), New(uint32(2), uint32(0), uint32(1)).PrevIdx())
+	})
+}
+
+func TestNode_CharIdx(t *testing.T) {
+	t.Run("алфавит uint8 морфемы uint16", func(t *testing.T) {
+		require.Equal(t, uint8(2), New(uint16(1), uint8(2), uint16(0)).CharIdx())
+	})
+	t.Run("алфавит uint8 морфемы uint32", func(t *testing.T) {
+		require.Equal(t, uint8(2), New(uint32(1), uint8(2), uint32(0)).CharIdx())
+	})
+	t.Run("алфавит uint16 морфемы uint16", func(t *testing.T) {
+		require.Equal(t, uint16(2), New(uint16(1), uint16(2), uint16(0)).CharIdx())
+	})
+	t.Run("алфавит uint16 морфемы uint32", func(t *testing.T) {
+		require.Equal(t, uint16(2), New(uint32(1), uint16(2), uint32(0)).CharIdx())
+	})
+	t.Run("алфавит uint32 морфемы uint16", func(t *testing.T) {
+		require.Equal(t, uint16(2), New(uint16(1), uint16(2), uint16(0)).CharIdx())
+	})
+	t.Run("алфавит uint32 морфемы uint32", func(t *testing.T) {
+		require.Equal(t, uint32(2), New(uint32(1), uint32(2), uint32(0)).CharIdx())
+	})
+}
+
+func TestNode_HasNext(t *testing.T) {
+	t.Run("алфавит uint8 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint8(0), uint16(0))
+		_ = node.Next(2, 1)
+		require.True(t, node.HasNext(1))
+		require.False(t, node.HasNext(0))
+	})
+	t.Run("алфавит uint8 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint8(0), uint32(0))
+		_ = node.Next(2, 1)
+		require.True(t, node.HasNext(1))
+		require.False(t, node.HasNext(0))
+	})
+	t.Run("алфавит uint16 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint16(0), uint16(0))
+		_ = node.Next(2, 1)
+		require.True(t, node.HasNext(1))
+		require.False(t, node.HasNext(0))
+	})
+	t.Run("алфавит uint16 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint16(0), uint32(0))
+		_ = node.Next(2, 1)
+		require.True(t, node.HasNext(1))
+		require.False(t, node.HasNext(0))
+	})
+	t.Run("алфавит uint32 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint32(0), uint16(0))
+		_ = node.Next(2, 1)
+		require.True(t, node.HasNext(1))
+		require.False(t, node.HasNext(0))
+	})
+	t.Run("алфавит uint32 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint32(0), uint32(0))
+		_ = node.Next(2, 1)
+		require.True(t, node.HasNext(1))
+		require.False(t, node.HasNext(0))
+	})
+}
+
+func TestNode_NextIdx(t *testing.T) {
+	t.Run("алфавит uint8 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint8(0), uint16(0))
+		_ = node.Next(2, 1)
+		nextIdx, exists := node.NextIdx(1)
+		require.True(t, exists)
+		require.Equal(t, uint16(2), nextIdx)
+		nextIdx, exists = node.NextIdx(2)
+		require.False(t, exists)
+	})
+	t.Run("алфавит uint8 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint8(0), uint32(0))
+		_ = node.Next(2, 1)
+		nextIdx, exists := node.NextIdx(1)
+		require.True(t, exists)
+		require.Equal(t, uint32(2), nextIdx)
+		nextIdx, exists = node.NextIdx(2)
+		require.False(t, exists)
+	})
+	t.Run("алфавит uint16 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint16(0), uint16(0))
+		_ = node.Next(2, 1)
+		nextIdx, exists := node.NextIdx(1)
+		require.True(t, exists)
+		require.Equal(t, uint16(2), nextIdx)
+		nextIdx, exists = node.NextIdx(2)
+		require.False(t, exists)
+	})
+	t.Run("алфавит uint16 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint16(0), uint32(0))
+		_ = node.Next(2, 1)
+		nextIdx, exists := node.NextIdx(1)
+		require.True(t, exists)
+		require.Equal(t, uint32(2), nextIdx)
+		nextIdx, exists = node.NextIdx(2)
+		require.False(t, exists)
+	})
+	t.Run("алфавит uint32 морфемы uint16", func(t *testing.T) {
+		node := New(uint16(1), uint32(0), uint16(0))
+		_ = node.Next(2, 1)
+		nextIdx, exists := node.NextIdx(1)
+		require.True(t, exists)
+		require.Equal(t, uint16(2), nextIdx)
+		nextIdx, exists = node.NextIdx(2)
+		require.False(t, exists)
+	})
+	t.Run("алфавит uint32 морфемы uint32", func(t *testing.T) {
+		node := New(uint32(1), uint32(0), uint32(0))
+		_ = node.Next(2, 1)
+		nextIdx, exists := node.NextIdx(1)
+		require.True(t, exists)
+		require.Equal(t, uint32(2), nextIdx)
+		nextIdx, exists = node.NextIdx(2)
+		require.False(t, exists)
+	})
+}
